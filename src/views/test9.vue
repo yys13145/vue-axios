7a5bf935369e4f2b10f5db413a9e04c8b9af8414
yangyisong <1091139789@qq.com>
2021-01-21 15:12:46 +0800

    增加取消请求

---
+++ src/views/test9.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test9">
+    <p>axios取消请求</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios' 
+export default {
+  name: 'test9',
+  components: {
+    
+  },
+  created(){
+     
+  }
+
+  
+}
+</script>
